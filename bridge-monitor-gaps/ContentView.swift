e8ea5f48d6074d636ca1f476a6116e96b1bf7049
Eric Ferreira <2483476+ericbf@users.noreply.github.com>
2020-05-15 18:07:00 -0400

    Initial Commit

---
+++ bridge-monitor-gaps/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  bridge-monitor-gaps
+//
+//  Created by Eric Ferreira on 5/15/20.
+//  Copyright © 2020 ferreira.life. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
